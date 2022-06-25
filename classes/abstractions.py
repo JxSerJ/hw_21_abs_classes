3c9e5098da8c0dce3b249ed5639e3c89d7b0ac67
S.P <sergey.a.protsenko@mail.ru>
2022-06-25 20:58:17 +0700

    refactored, abstractions supplemented with additional method

--- classes/abstractions.py
+++ classes/abstractions.py
@@ -16,6 +16,7 @@ class Storage(ABC):
     def add(self, item_name: str, item_quantity: int):
         """
         Adds determined quantity of items into storage based on item name and quantity.
+
         :param item_name: item name which will be added
         :param item_quantity: item quantity which will be added
         :return: None
@@ -26,8 +27,19 @@ class Storage(ABC):
     def remove(self, item_name: str, item_quantity: int):
         """
         Removes determined quantity of items from storage based on item name and quantity.
+
         :param item_name: item name which will be removed
         :param item_quantity: item quantity which will be removed
+        :return: None
+        """
+        pass
+
+    @abstractmethod
+    def remove_item_entirely(self, item_name: str):
+        """
+        Removes item from storage database. Works only if item quantity = 0
+
+        :param item_name: Name of the item which you want to remove form storage
         :return: None
         """
         pass
@@ -36,6 +48,7 @@ class Storage(ABC):
     def get_free_space(self) -> int:
         """
         Returns storage free space
+
         :return: int storage free space
         """
         pass
@@ -44,6 +57,7 @@ class Storage(ABC):
     def get_items(self) -> dict[str, int]:
         """"
         Returns storage content
+
         :return: storage content in dict {item name: quantity}
         """
         pass
@@ -52,6 +66,7 @@ class Storage(ABC):
     def get_unique_items_count(self) -> int:
         """
         Returns unique items count
+
         :return: int count of items
         """
         pass
